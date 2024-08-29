42bf8d088a2b3353edef86acec860a819aab7c5c
ImSurprisable <toastedflqme@gmail.com>
2024-08-28 20:39:16 -0700

    grid highlighting system

---
+++ PVZ1 Remake/Assets/Scripts/PlantGrid.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class PlantGrid : MonoBehaviour
+{
+    [SerializeField] private Transform cellPrefab;
+
+    [Space]
+
+    [Tooltip("Width & height of the grid in cells")]
+    [SerializeField] private Vector2Int gridDimensions;
+
+    [Space]
+
+    [Tooltip("Top-left corner of the grid")]
+    [SerializeField] private Vector2 origin;
+    [Tooltip("Width & height of each individual cell")]
+    [SerializeField] private Vector2 cellDimensions;
+
+    
+
+    private PlantGridCell[][] gridCells;
+    private Vector2Int hoveredCell = new Vector2Int(-1, -1);
+
+    private GameObject[] highlightRows;
+    private GameObject[] highlightCols;
+
+
+
+
+    private void Awake()
+    {
+        InitializeGrid();
+    }
+
+
+    private void Update()
+    {
+        UpdateHoveredCell();
+    }
+
+
+    private void InitializeGrid()
+    {
+        gridCells = new PlantGridCell[gridDimensions.x][];
+        highlightCols = new GameObject[gridDimensions.x];
+        highlightRows = new GameObject[gridDimensions.y];
+
+        for (int gridX = 0; gridX < gridDimensions.x; gridX++)
+        {
+            GameObject highlightCol = GameObject.Instantiate(cellPrefab, new Vector2(cellDimensions.x*gridX + cellDimensions.x/2, cellDimensions.y*gridDimensions.y/2) + origin, Quaternion.identity, transform).gameObject;
+            highlightCol.transform.localScale = new Vector3(cellDimensions.x, cellDimensions.y * gridDimensions.y);
+            highlightCol.SetActive(false);
+            highlightCols[gridX] = highlightCol;
+
+            gridCells[gridX] = new PlantGridCell[gridDimensions.y];
+            for (int gridY = 0; gridY < gridDimensions.y; gridY++)
+            {
+                if (gridX == 0)
+                {
+                    GameObject highlightRow = GameObject.Instantiate(cellPrefab, new Vector2(cellDimensions.x*gridDimensions.x/2, cellDimensions.y*gridY + cellDimensions.y/2) + origin, Quaternion.identity, transform).gameObject;
+                    highlightRow.transform.localScale = new Vector3(cellDimensions.x * gridDimensions.x, cellDimensions.y);
+                    highlightRow.SetActive(false);
+                    highlightRows[gridY] = highlightRow;
+                }
+                Vector2 position = new Vector2(cellDimensions.x * gridX, cellDimensions.y * gridY) + cellDimensions/2 + origin;
+                gridCells[gridX][gridY] = new PlantGridCell(position, cellDimensions.x, cellDimensions.y);
+            }
+        }
+    }
+
+
+
+    private void UpdateHoveredCell() 
+    {
+        bool found = false;
+        Vector2Int foundCell = new Vector2Int(-1, -1);
+        for (int gridX = 0; gridX < gridDimensions.x; gridX++)
+        {
+            for (int gridY = 0; gridY < gridDimensions.y; gridY++)
+            {
+                if (gridCells[gridX][gridY].IsHovered(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+                {
+                    found = true;
+                    foundCell = new Vector2Int(gridX, gridY);
+                    break;
+                }
+            }
+            if (found) break;
+        }
+        
+        if (foundCell.Equals(hoveredCell)) return;
+        if (!hoveredCell.Equals(new Vector2Int(-1, -1))) {
+            highlightCols[hoveredCell.x].SetActive(false);
+            highlightRows[hoveredCell.y].SetActive(false);
+        }
+        if (found)
+        {
+            hoveredCell = foundCell;
+            highlightCols[hoveredCell.x].SetActive(true);
+            highlightRows[hoveredCell.y].SetActive(true);
+            return;
+        }
+        hoveredCell = new Vector2Int(-1, -1);
+    }
+
+}
---
+++ PVZ1 Remake/Assets/Scripts/PlantGridCell.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlantGridCell
+{
+    private float width;
+    private float height;
+    private Vector2 worldPosition;
+
+    public PlantGridCell(Vector2 worldPosition, float width, float height) 
+    {
+        this.width = width;
+        this.height = height;
+        this.worldPosition = worldPosition;
+    }
+
+    public bool IsHovered(Vector2 mousePos) {
+        float left = worldPosition.x - width/2;
+        float right = worldPosition.x + width/2;
+        float bottom = worldPosition.y - height/2;
+        float top = worldPosition.y + height/2;
+
+        return mousePos.x >= left && mousePos.x < right && mousePos.y >= bottom && mousePos.y < top;
+    }
+
+}
